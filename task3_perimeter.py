be5f2cdd583d938ea3604644fda54ddc64f8e6e1
chodorkarolina-art <chodor.karolina@gmail.com>
2026-04-09 21:49:24 +0200

    Delete task3_perimeter.py

--- task3_perimeter.py
+++
@@ -1,8 +0,0 @@
-lenght = float(input("Podaj długość prostokąta: "))
-widith = float(input("Podaj szerokość prostokąta: "))
-
-perimeter = 2 * (lenght + widith)
-
-result = (perimeter)
-
-print(perimeter)
