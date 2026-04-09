53f34db497679b185fa6cbc48303325bcd24fa6c
chodorkarolina-art <chodor.karolina@gmail.com>
2026-04-09 21:44:08 +0200

    Delete task10_access.py

--- task10_access.py
+++
@@ -1,6 +0,0 @@
-height = input("Podaj swój wzrost w cm: ")
-guardian = input("Czy masz opiekuna? (tak/nie): ").lower()
-
-can_enter = (height >= "120" and guardian == "tak") or height >= "160"
-
-print(f"Możesz wejść na karuzelę: {can_enter}")
