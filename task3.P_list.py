8eb6367c168ca40ca0eb127fa2967c58a0ea5a09
chodorkarolina-art <chodor.karolina@gmail.com>
2026-04-09 21:46:48 +0200

    Delete task3.P_list.py

--- task3.P_list.py
+++
@@ -1,6 +0,0 @@
-temps =  [12, 15, 14, 18, 20, 19, 24, 21, 18, 17, 24]
-rains = [False, False, True, False, True, False, False, True, True, False, False]
-
-weather_data = [{"temp": t, "rain": r} for t, r in zip(temps,rains)]
-
-print(weather_data)
