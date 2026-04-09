ff8c80238ce191cdc7a34938145406278ab040fc
chodorkarolina-art <chodor.karolina@gmail.com>
2026-04-09 21:50:16 +0200

    Delete task6_converter.py

--- task6_converter.py
+++
@@ -1,7 +0,0 @@
-name = int(input("Podaj liczbę całkowitą: "))
-
-binary = bin(name)
-hexadecimal = hex(name)
-
-print(f"Liczba {name} w systemie binarnym to: {binary}")
-print(f"Liczba {name} w systemie szesnastkowym to: {hexadecimal}")
