c65dfa8e70a0062c683127e942408f1ee2f9a8e2
chodorkarolina-art <chodor.karolina@gmail.com>
2026-05-30 13:07:36 +0200

    Delete lesson2 directory

--- lesson2/lesson2_task7.py
+++
@@ -1,8 +0,0 @@
-# Utwórz krotkę punkt = (10, 20, 30) .
-# Spróbuj zmienić pierwszy element krotki на 15 .
-# Wyjaśnij w komentarzu do kodu, dlaczego wystąpił błąd.
-
-punkt = (10,10,20)
-punkt[0] = 15
-
-# to są tuple niemodyfikowane, nie można zmieniać elementów. 
