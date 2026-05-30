2987e8109874cd49dcd6bb21f6fa66617ab3fdda
chodorkarolina-art <chodor.karolina@gmail.com>
2026-05-30 13:11:56 +0200

    Add files via upload

---
+++ lesson1/lesson1_task1_story.py
@@ -0,0 +1,4 @@
+print("Mam na imię Karolina" )
+print("Moim hobby jest żeglarstwo" )
+print("Uczę się Pythona ponieważ chce poznac inny rodzaj projektowania niż architektura wnętrz" )
+print("Chcę stworzyć aplikację związaną z błędami w projetowaniu wnętrz ;-)" )
