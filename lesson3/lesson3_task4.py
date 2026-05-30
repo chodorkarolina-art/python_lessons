59a79ad0a5f083394d8502ae591a254906f28f97
chodorkarolina-art <chodor.karolina@gmail.com>
2026-05-30 13:32:11 +0200

    Delete lesson3 directory

--- lesson3/lesson3_task4.py
+++
@@ -1,6 +0,0 @@
-# Wejście w jednej linii: Użyj funkcji print() i jej parametru end , 
-# aby zadać pytanie i pozwolić użytkownikowi odpowiedzieć w tej samej linii. 
-# Przykład: Podaj swoje imię:n[tutaj użytkownik wpisuje].
-
-imie = input("Podaj swoje imie: ")
-
