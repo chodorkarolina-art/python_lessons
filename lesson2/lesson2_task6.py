c5d421929c38403c990a565f122a7646aa0d6d5a
chodorkarolina-art <chodor.karolina@gmail.com>
2026-06-05 12:43:19 +0200

    Add files via upload

--- lesson2/lesson2_task6.py
+++ lesson2/lesson2_task6.py
@@ -2,7 +2,7 @@
 # Spróbuj zmienić pierwszy element krotki на 15 .
 # Wyjaśnij w komentarzu do kodu, dlaczego wystąpił błąd.
 
-punkt = (10,10,20)
+punkt = (10,20,30)
 punkt[0] = 15
 
 # to są tuple niemodyfikowane, nie można zmieniać elementów. Jeślli chcemy ją zmienić musimy utworzyć nową. 
