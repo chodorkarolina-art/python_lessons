a4e1c588437cc4bae57570d0831a827eb24eedee
chodorkarolina-art <chodor.karolina@gmail.com>
2026-06-15 19:17:10 +0200

    Delete lesson6 directory

--- lesson6/lesson6_task6.py
+++
@@ -1,3 +0,0 @@
-# Dekorator logujący: Napisz dekorator @loguj , który przed wywołaniem udekorowanej
-# funkcji wypisze komunikat Uruchamiam funkcję [nazwa_funkcji]... , a po jej
-# zakończeniu Zakończono funkcję [nazwa_funkcji].
--- lesson6/lesson6_task8.py
+++
@@ -1,2 +0,0 @@
-# Dekorator z argumentem: Stwórz dekorator @powtorz(n) , który przyjmuje argument n 
-# i powoduje, że udekorowana funkcja zostanie wykonana n razy.
