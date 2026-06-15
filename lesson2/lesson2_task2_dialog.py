dccb674c8a307c874f813e473c22f53296f03337
chodorkarolina-art <chodor.karolina@gmail.com>
2026-06-15 22:30:36 +0200

    Add files via upload

---
+++ lesson2/lesson2_task2_dialog.py
@@ -0,0 +1,5 @@
+name = input("Jak masz na imię? ")
+age = input ("Ile masz lat? ")
+town = input ("Gdzie mieszkasz? ")
+
+print(f"A więc, masz na imię {name}, masz {age} lat, mieszkasz w {town} ")
---
+++ lesson2/lesson2_task3_perimeter.py
@@ -0,0 +1,8 @@
+lenght = float(input("Podaj długość prostokąta: "))
+widith = float(input("Podaj szerokość prostokąta: "))
+
+perimeter = 2 * (lenght + widith)
+
+result = (perimeter)
+
+print(perimeter)
